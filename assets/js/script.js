9a5911faf2a006b997d1e672fb8bd2dddc4a72fd
Maya Morales <mayainomorales@gmail.com>
2022-06-16 00:02:08 -0700

    instantiating html variables and fixing file structure

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+// variables that will reference html elements
+
+var start = document.querySelector('#start');
+var timer = document.getElementById('timer');
+var score = document.querySelector('#score');
+var instructions = document.querySelector('#instructions');
+var quiz = document.querySelector('#quiz');
+var questionNum = document.querySelector('#questions-given');
+var end = document.querySelector('#end');
+var initials = document.querySelector('#initials');
+var highscore = document.querySelector('#highscore');
+var highscores = document.querySelector('#highscores-list');
